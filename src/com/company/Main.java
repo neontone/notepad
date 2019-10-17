545306075187bbf21e41d2810cbe8f955c95f3ca
neontone <antons.korenevskis@gmail.com>
2019-10-17 19:35:26 +0300

    save and list func

---
+++ src/com/company/Main.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    static ArrayList<Person> records = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        System.out.println("Enter a command");
+        for (; ; ) {
+            System.out.print("> ");
+            String cmd = scan.next();
+
+            switch (cmd) {
+                case "help":
+                    showHelp();
+                    break;
+                case "exit":
+                    System.out.println("Good bye!");
+                    return;
+                case "create":
+                    createRecord();
+                    break;
+                case "list":
+                    listRecords();
+                    break;
+                default:
+                    System.out.println("Wrong command");
+                    break;
+            }
+        }
+
+    }
+
+
+
+    private static void listRecords() {
+        for (Person p : records) {
+            System.out.printf("%d %s %s %s\n",
+                    p.getId(),
+                    p.getName(),
+                    p.getSurname(),
+                    p.getPhone());
+        }
+    }
+
+    private static void createRecord() {
+        System.out.print("type> ");
+        String type = scan.next();
+        switch (type) {
+            case "person":
+                createPerson();
+                break;
+            default:
+                System.out.println("Error. Unknown record type");
+        }
+    }
+
+    private static void createPerson() {
+        System.out.println("Name> ");
+        String name = scan.next();
+        System.out.println("Surname> ");
+        String surname = scan.next();
+        System.out.println("Phone> ");
+        String phone = scan.next();
+
+        Person p = new Person();
+        p.setName(name);
+        p.setSurname(surname);
+        p.setPhone(phone);
+
+        records.add(p);
+    }
+
+    private static void showHelp() {
+        System.out.println("One day here will be a great manual. Eventually");
+    }
+
+
+}
+
+
+
---
+++ src/com/company/Person.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Person {
+   private int id;
+   private String name;
+   private String surname;
+   private String phone;
+
+    public static void add(Person p) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
